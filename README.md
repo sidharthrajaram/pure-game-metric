b39fceeef8d855a6890d32235ac4f36357aadc0c
sidharthrajaram <sidharthrajaram@gmail.com>
2019-07-29 14:32:26 -0700

    docs

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# pure-game-metric
-A novel approach to rating a team's regular season performance
+# The Pure Game Metric (PGM)
+A novel approach to rating a team's regular season performance.
+
+#### Notes:
+
+- Planning on adding consideration for pace of play (to adjust for eras)
+
+### Pure Game Metric Explained
+This portion explains how a team's PGM is calculated and shows some examples.
+![great teams](img/pgm.png?raw=true "great teams")
+![departure of goats](img/pgm2.png?raw=true "goats leaving")
--- game_improved.py
+++ game_improved.py
@@ -1,7 +1,3 @@
-# more efficient, better version of game_basic.py
-# will be used to predict game results each day
-
-import random
 from playground import get_team_data, generate_dataframe, clean_up_df
 
 
@@ -36,51 +32,11 @@ def get_team_metric(team_id, year):
     return calculate_metric(getTeamDf(team_id, year))
 
 
-# t1 is home team, t2 is away team, year parameters are strings
-def simulate(t1_year, t2_year, t1_id, t2_id, epochs=1000, home_variation_max=100, away_variation_max=85):
-    print("Simulation Presets:")
-    print("Epochs: {}".format(epochs))
-    print("Home Team Variation Range Max: {}".format(home_variation_max))
-    print("Away Team Variation Range Max: {}".format(away_variation_max))
-    print()
-
-    t1_metric = get_team_metric(t1_id, t1_year)
-    t2_metric = get_team_metric(t2_id, t2_year)
-
-    print('{} has a pure game metric of {}'.format(t1_id, t1_metric))
-    print('{} has a pure game metric of {}'.format(t2_id, t2_metric))
-    print()
-    t1_wins = 0
-    t2_wins = 0
-
-    for i in range(epochs):
-        t1_random_variation = random.randint(0, home_variation_max) / 10  # parameter these
-        t2_random_variation = random.randint(0, away_variation_max) / 10   # parameter these
-        t1_final_metric = t1_metric + t1_random_variation
-        t2_final_metric = t2_metric + t2_random_variation
-
-        if t1_final_metric > t2_final_metric:
-            t1_wins +=1
-        else:
-            t2_wins +=1
-
-    print('In {} simulated games, {}: {} wins, {}: {} wins'.format(epochs, t1_id, t1_wins, t2_id, t2_wins))
-    if t1_wins > t2_wins:
-        return t1_year, t1_id, float(t1_wins)/float(epochs/100)
-    else:
-        return t2_year, t2_id, float(t2_wins)/float(epochs/100)
-
-
 if __name__ == '__main__':
 
-    t1_year = input("Year of Home Team: ")
-    t1_id = input("ID of Home Team: ")
-    t2_year = input("Year of Away Team: ")
-    t2_id = input("ID of Away Team: ")
-    print()
-    win_team_year, win_team_id, win_team_percent = simulate(t1_year, t2_year, t1_id, t2_id)
-    print()
-    print("The {} {} team has a {}% chance of winning.".format(win_team_year, win_team_id, win_team_percent))
+    team = input('team id: ')
+    year = input('year: ')
+    print(get_team_metric(team, year))
 
 
 
